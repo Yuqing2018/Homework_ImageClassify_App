8eb7223a5a10f2511a54d2b49af0d066258a285a
yuqing <v-yuqw@microsoft.com>
2018-11-20 14:01:20 +0800

    upload the source code

---
+++ Homework_ImageClassify_App/ImageClassifyHelper.cs
@@ -0,0 +1,211 @@
+﻿using Baidu.Aip;
+using Baidu.Aip.ImageClassify;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Homework_ImageClassify_App
+{
+    public class ImageClassifyHelper
+    {
+        // 设置APPID/AK/SK
+        private const string APP_ID = "14817956";
+        private const string API_KEY = "HBKWxOKk7wlqvoY6ZdBY9xZE";
+        private const string SECRET_KEY = "BOXpiPQGXwVzO4zpDqUt3kxKhuzeBrI2";
+
+        private static ImageClassify _imageClient = new ImageClassify(API_KEY, SECRET_KEY) { Timeout = 6000 };
+
+        //用户向服务请求检测图像中的主体位置。
+        public static JObject ObjectDetectDemo(byte[] image, Dictionary<string,object> options)
+        {
+            try
+            {
+                // 带参数调用图像主体检测
+                return _imageClient.ObjectDetect(image, options);
+            }
+            catch (AipException exp)
+            {
+                MessageBox.Show(exp.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return null;
+        }
+
+        //通用物体识别
+        //该请求用于通用物体及场景识别，即对于输入的一张图片（可正常解码，且长宽比适宜），输出图片中的多个物体及场景标签。
+        public static JObject AdvancedGeneralDemo(byte[] image)
+        {
+            try
+            {
+                // 调用通用物体识别，可能会抛出网络等异常，请使用try/catch捕获
+                //return _imageClient.AdvancedGeneral(image);
+                // 如果有可选参数
+                var options = new Dictionary<string, object>{
+        {"baike_num", 5}
+    };
+                // 带参数调用通用物体识别
+                return _imageClient.AdvancedGeneral(image, options);
+            }
+            catch (AipException exp)
+            {
+                MessageBox.Show(exp.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return null;
+        }
+        public static JObject DishDetectDemo(byte[] image)
+        {
+            try
+            {
+                // 调用通用物体识别，可能会抛出网络等异常，请使用try/catch捕获
+                //return _imageClient.DishDetect(image);
+                // 如果有可选参数
+                var options = new Dictionary<string, object>{
+        {"top_num", 3},
+        {"filter_threshold", "0.7"},
+        {"baike_num", 5}
+    };
+
+                // 带参数调用菜品识别
+                return _imageClient.DishDetect(image, options);
+            }
+            catch (AipException exp)
+            {
+                MessageBox.Show(exp.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return null;
+        }
+
+        public static JObject CarDetectDemo(byte[] image)
+        {
+            try
+            {
+                // 调用通用物体识别，可能会抛出网络等异常，请使用try/catch捕获
+                //return _imageClient.CarDetect(image);
+                // 如果有可选参数
+                var options = new Dictionary<string, object>{
+        {"top_num", 3},
+        {"baike_num", 5}
+    };
+                // 带参数调用车辆识别
+
+                return _imageClient.CarDetect(image, options);
+            }
+            catch (AipException exp)
+            {
+                MessageBox.Show(exp.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return null;
+        }
+        public static JObject LogoDetectDemo(byte[] image)
+        {
+            try
+            {
+                // 调用通用物体识别，可能会抛出网络等异常，请使用try/catch捕获
+                //return _imageClient.LogoSearch(image);
+                // 如果有可选参数
+                var options = new Dictionary<string, object>{
+        {"custom_lib", "true"}
+    };
+                // 带参数调用logo商标识别
+                return _imageClient.LogoSearch(image, options);
+            }
+            catch (AipException exp)
+            {
+                MessageBox.Show(exp.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return null;
+        }
+
+        public static JObject AnimalDetectDemo(byte[] image)
+        {
+            try
+            {
+                // 调用通用物体识别，可能会抛出网络等异常，请使用try/catch捕获
+                //return _imageClient.AnimalDetect(image);
+                // 如果有可选参数
+                var options = new Dictionary<string, object>{
+        {"top_num", 3},
+        {"baike_num", 5}
+    };
+                // 带参数调用动物识别
+                return _imageClient.AnimalDetect(image, options);
+            }
+            catch (AipException exp)
+            {
+                MessageBox.Show(exp.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 植物识别
+        /// </summary>
+        /// <param name="flePath"></param>
+        /// <returns></returns>
+        public static JObject PlantDetectDemo(byte[] image)
+        {
+            try
+            {
+                // 调用通用物体识别，可能会抛出网络等异常，请使用try/catch捕获
+                //return _imageClient.PlantDetect(image);
+                // 如果有可选参数
+                var options = new Dictionary<string, object>{
+        {"baike_num", 5}
+    };
+                // 带参数调用植物识别
+                return _imageClient.PlantDetect(image, options);
+            }
+            catch (AipException exp)
+            {
+                MessageBox.Show(exp.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return null;
+        }
+    }
+
+    public class LocationInfo
+    {
+        public float left { get; set; }
+        public float top { get; set; }
+        public float width { get; set; }
+        public float height { get; set; }
+    }
+
+    public class ImageClassifyResult<T>
+    {
+        public long log_id { get; set; }
+        public int result_num { get; set; }
+        public List<T> result { get; set; }
+    }
+
+    public class DishItem
+    {
+        public string name { get; set; }
+        public double calorie { get; set; }
+        public double probability { get; set; }
+    }
+
+    public class CarItem
+    {
+        public string name { get; set; }
+        public double score { get; set; }
+        public string year { get; set; }
+        public string keyword { get; set; }
+        public BaikeInfo baike_info { get; set; }
+    }
+
+    public class LogoItem
+    {
+        public string name { get; set; }
+        public int type { get; set; }
+        public double probability { get; set; }
+        public LocationInfo location { get; set; }
+    }
+
+    public class BaikeInfo {
+        public string baike_url { get; set; }
+        public string image_url { get; set; }
+        public string description { get; set; }
+    }
+
+}
---
+++ Homework_ImageClassify_App/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using SWF = System.Windows.Forms;
+using SD = System.Drawing;
+using Newtonsoft.Json;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Linq;
+
+namespace Homework_ImageClassify_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private byte[] ImageBytes;
+        private LocationInfo location;
+        public MainWindow()
+        {
+            InitializeComponent();
+            featureSelect_LV.ItemsSource = Enum.GetValues(typeof(FeatureType)).Cast<FeatureType>().ToList();
+            Image_PB.Paint += Picturebox_Paint;
+            DectectFace_CB.Checked += DectectFace_CB_Checked;
+            DectectFace_CB.Unchecked += DectectFace_CB_Checked;
+        }
+
+        private void Picturebox_Paint(object sender, SWF.PaintEventArgs e)
+        {
+            if (location != null)
+                e.Graphics.DrawRectangle(SD.Pens.Red, location.left, location.top, location.width, location.height);
+        }
+
+        private void UploadImage_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            SWF.OpenFileDialog dlg = new SWF.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "Image Files|*.jpg;*.png;*.bmp;*.jpeg;*.gif;";
+
+            // Get the selected file name and display in a TextBox 
+            if (dlg.ShowDialog() == SWF.DialogResult.OK)
+            {
+                Image_PB.Image = SD.Image.FromFile(dlg.FileName);
+                ImageBytes = File.ReadAllBytes(dlg.FileName);
+
+                ObjectDected();
+                
+                //填写
+                var result = ImageClassifyHelper.AdvancedGeneralDemo(ImageBytes);
+                var info = JsonConvert.DeserializeObject<ImageClassifyResult<CarItem>>(result.ToString());
+                List<string> items = new List<string>();
+                info.result.ForEach(item => { items.Add(String.Format("{0}\n{1}\n", item.keyword, item?.baike_info?.description)); });
+
+                SetRichTextBoxValue(relationInfo_Rtb, items);
+            }
+        }
+
+        private void ObjectDected()
+        {
+            String with_face = "0";
+            if (DectectFace_CB.IsChecked ?? false)
+                with_face = "1";
+            var options = new Dictionary<string, object>()
+            {
+                {"with_face", with_face}
+            };
+            var imageResult = ImageClassifyHelper.ObjectDetectDemo(ImageBytes, options);
+            location = JsonConvert.DeserializeObject<LocationInfo>(imageResult["result"].ToString());
+            ResetLocation(location, Image_PB.Image.Width, Image_PB.Image.Height);
+        }
+
+        private void ResetLocation(LocationInfo location, float imageWidth, float imageHeight)
+        {
+            if (Image_PB.Image != null)
+            {
+                var scaleX = Image_PB.Width / imageWidth;
+                var scaleY = Image_PB.Height / imageHeight;
+                float scale = scaleX > scaleY ? (float)scaleY : (float)scaleX;
+                location.width *= scale;
+                location.height *= scale;
+                location.left *= scale;
+                location.top *= scale;
+                location.left += (Image_PB.Width - imageWidth * scale) / 2;
+                location.top += (Image_PB.Height - imageHeight * scale ) / 2;
+            }
+            Image_PB.Refresh();
+        }
+        
+        private void feature_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button btn)
+            {
+                List<string> items = new List<string>();
+                switch (btn.DataContext)
+                {
+                   case FeatureType.菜品识别:
+                        var dishResult = ImageClassifyHelper.DishDetectDemo(ImageBytes);
+                        var dishDectectResult = JsonConvert.DeserializeObject<ImageClassifyResult<DishItem>>(dishResult.ToString());
+                        dishDectectResult.result.ForEach(item => { items.Add(String.Format("菜名：{0}\n 卡路里：{1}\n 置信度：{2}", item.name, item.calorie, item.probability)); });
+                        break;
+                    case FeatureType.车型识别:
+                        var carResult = ImageClassifyHelper.CarDetectDemo(ImageBytes);
+                        var carDectectResult = JsonConvert.DeserializeObject<ImageClassifyResult<CarItem>>(carResult.ToString());
+                        carDectectResult.result.ForEach(item => { items.Add(String.Format("车型：{0}\n 年份：{1}\n 置信度：{2}", item.name, item.year, item.score)); });
+                        break;
+                    case FeatureType.商标识别:
+                        var logoResult = ImageClassifyHelper.LogoDetectDemo(ImageBytes);
+                        var logoDectectResult = JsonConvert.DeserializeObject<ImageClassifyResult<LogoItem>>(logoResult.ToString());
+                        logoDectectResult.result.ForEach(item => { items.Add(String.Format("商标：{0}\n 置信度：{1}", item.name, item.probability)); });
+                        break;
+                    case FeatureType.动物识别:
+                        var animalResult = ImageClassifyHelper.AnimalDetectDemo(ImageBytes);
+                        var animalDectectResult = JsonConvert.DeserializeObject<ImageClassifyResult<CarItem>>(animalResult.ToString());
+                        animalDectectResult.result.ForEach(item => { items.Add(String.Format("动物：{0}\n 置信度：{1}", item.name, item.score)); });
+                        break;
+                    case FeatureType.植物识别:
+                        var plantResult = ImageClassifyHelper.PlantDetectDemo(ImageBytes);
+                        var plantDectectResult = JsonConvert.DeserializeObject<ImageClassifyResult<CarItem>>(plantResult.ToString());
+                        plantDectectResult.result.ForEach(item => { items.Add(String.Format("植物：{0}\n 置信度：{1}\n", item.name, item.score)); });
+                        break;
+                }
+                SetRichTextBoxValue(detectResult_Rtb, items);
+            }
+        }
+
+        private void SetRichTextBoxValue(object sender, List<String> texts)
+        {
+            if(sender is RichTextBox rtb)
+            {
+                rtb.Document.Blocks.Clear();//清空对象text
+                texts.ForEach(item =>
+                {
+                    Run runx = new Run(item);
+                    Paragraph paragraph = new Paragraph(runx);
+                    rtb.Document.Blocks.Add(paragraph);
+                });
+
+            }
+            
+        }
+        private void DectectFace_CB_Checked(object sender, RoutedEventArgs e)
+        {
+            var flag = DectectFace_CB.IsChecked;
+                //= !DectectFace_CB.IsChecked;
+            if (Image_PB.Image != null)
+                ObjectDected();
+        }
+    }
+
+    public enum FeatureType
+    {
+        菜品识别 = 1,
+        车型识别 = 2,
+        商标识别 = 3,
+        动物识别 = 4,
+        植物识别 = 5,
+    }
+}
